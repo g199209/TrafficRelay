4a4559616285cfe440d720aaedac71567cf685e4
Gao Mingfei <mingfei.gao@ucloud.cn>
2021-05-24 11:45:01 +0800

    [Enhance] Add build script.
    Signed-off-by: Gao Mingfei <mingfei.gao@ucloud.cn>

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if test -e build;then
+    echo "build dir already exists; rm -rf build and re-run"
+    rm -rf build
+fi
+
+mkdir build
+cd build
+cmake ../ -DCMAKE_BUILD_TYPE=Release
+make -j8
